0f2db1442d912ca6937f1e4e75adc4db48bf696a
Zuza Sawala <zuzanna.sawala@gmail.com>
2016-10-05 20:55:41 +0200

    Transformations
    Added transformation with success and multiple same models

--- tankSim/main.cpp
+++ tankSim/main.cpp
@@ -2,6 +2,9 @@
 #include "SOIL.h"
 #include "GL/glew.h"
 #include "GL/glut.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
 #include <stdio.h>
 #include "shaderprogram.h"
 
@@ -74,42 +77,46 @@ void displayFrame() {
 		// tutaj renderowanie
 
 	//koordynaty wierzcholkow
-	 GLfloat vertices[] = {
-        // Positions         // Colors
-        0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  1.0f, 1.0f,
-        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,	 1.0f, 0.0f,
-        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,		0.0f, 0.0f,
-		-0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,	0.0f, 1.0f,
+	GLfloat vertices[] = {
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f
     };
 
-	 GLuint indices[] = {  // Note that we start from 0!
-        0, 1, 3, // First Triangle
-        1, 2, 3  // Second Triangle
+	 glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
     };
 
-	 GLuint VBO, VAO, EBO;
+	 GLuint VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
     // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // Position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3* sizeof(GLfloat)));
-    glEnableVertexAttribArray(1);
-	glVertexAttribPointer(2, 2, GL_FLOAT,GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+	glVertexAttribPointer(2, 2, GL_FLOAT,GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(2); 
 
 	glBindVertexArray(0); // Unbind VAO
 
-	// tekstury
+	// TEKSTURY
 	   // Load and create a texture 
     GLuint texture;
     glGenTextures(1, &texture);
@@ -131,8 +138,54 @@ void displayFrame() {
     glBindVertexArray(0); // Unbind VAO
 	glBindTexture(GL_TEXTURE_2D, texture);
 	shaderProgram.Use();
+
+	// TRANSFORMACJE - PO URUCHOMIENIU SHADERA
+	/*glm::mat4 model;
+	model = glm::rotate(model, -70.0f, glm::vec3(1.0f, 0.0f, 0.0f)); 
+
+	glm::mat4 view;
+	// Note that we're translating the scene in the reverse direction of where we want to move
+	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -4.0f)); 
+
+	glm::mat4 projection;
+	projection = glm::perspective(45.0f, 800.0f / 800.0f, 0.1f, 100.0f);
+
+	GLint modelLoc = glGetUniformLocation(shaderProgram.Program, "model");
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+	modelLoc = glGetUniformLocation(shaderProgram.Program, "view");
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(view));
+	modelLoc = glGetUniformLocation(shaderProgram.Program, "projection");
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(projection));*/
+
+//	glm::mat4 model;
+	glm::mat4 view;
+        glm::mat4 projection;
+	//	model = glm::rotate(model, 40.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        projection = glm::perspective(45.0f, 800.0f / 800.0f, 0.1f, 100.0f);
+        // Get their uniform location
+        GLint modelLoc = glGetUniformLocation(shaderProgram.Program, "model");
+        GLint viewLoc = glGetUniformLocation(shaderProgram.Program, "view");
+        GLint projLoc = glGetUniformLocation(shaderProgram.Program, "projection");
+	//		 glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        // Pass the matrices to the shader
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        // Note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
 	glBindVertexArray(VAO);
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	for (GLuint i = 0; i < 10; i++)
+        {
+            // Calculate the model matrix for each object and pass it to shader before drawing
+            glm::mat4 model;
+            model = glm::translate(model, cubePositions[i]);
+            GLfloat angle = 20.0f * i;
+            model = glm::rotate(model, angle, glm::vec3(1.0f, 0.3f, 0.5f));
+            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+            glDrawArrays(GL_TRIANGLES, 0, 6);
+        }
+	glDrawArrays(GL_TRIANGLES, 0, 6);
 	glBindVertexArray(0); 
 	glutSwapBuffers();
 }
