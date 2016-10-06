d28d14636b8889184720b878ccc8bf550623bc22
Zuza Sawala <zuzanna.sawala@gmail.com>
2016-10-06 21:12:10 +0200

    Assimp and model files

--- tankSim/main.cpp
+++ tankSim/main.cpp
@@ -1,4 +1,6 @@
-﻿#include "windows.h"
+﻿#include <string>
+#include <vector>
+#include "windows.h"
 #include "SOIL.h"
 #include "GL/glew.h"
 #include "GL/glut.h"
@@ -9,6 +11,8 @@
 #include "glm/gtc/type_ptr.hpp"
 #include <stdio.h>
 #include "shaderprogram.h"
+#include "Model.h"
+#include "Mesh.h"
 const GLuint WIDTH = 1200, HEIGHT = 1000;
 
 GLuint VBO, VAO;
@@ -27,105 +31,7 @@ GLfloat lastFrame = 0.0f;  	// Time of last frame
 
 
 
-void displayFrame() {
-	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
-	Shader shaderProgram = Shader("vshader.txt","fshader.txt");
 
-	//Kompilacja vertex shader
-	
-		// tutaj renderowanie
-
-	//koordynaty wierzcholkow
-	GLfloat vertices[] = {
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f
-    };
-
-	 glm::vec3 cubePositions[] = {
-        glm::vec3( 0.0f,  0.0f,  0.0f),
-		 glm::vec3( 1.0f,  0.0f, -0.0f),
-		glm::vec3( 2.0f,  0.0f, -0.0f)
-
-    };
-
-	 GLuint VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // Position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
-    glEnableVertexAttribArray(0);
-	glVertexAttribPointer(2, 2, GL_FLOAT,GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(2); 
-
-	glBindVertexArray(0); // Unbind VAO
-
-	// TEKSTURY
-	   // Load and create a texture 
-    GLuint texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture); // All upcoming GL_TEXTURE_2D operations now have effect on this texture object
-    // Set the texture wrapping parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// Set texture wrapping to GL_REPEAT (usually basic wrapping method)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    // Set texture filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	int width, height;
-    unsigned char* image = SOIL_load_image("grass.png", &width, &height, 0, SOIL_LOAD_RGB);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-    glGenerateMipmap(GL_TEXTURE_2D);
-    SOIL_free_image_data(image);
-    glBindTexture(GL_TEXTURE_2D, 0); // Unbind texture when done, so we won't accidentily mess up our texture.
-
-    glBindVertexArray(0); // Unbind VAO
-	glBindTexture(GL_TEXTURE_2D, texture);
-	shaderProgram.Use();
-
-	glm::mat4 view;
-        glm::mat4 projection;
-	//	model = glm::rotate(model, 40.0f, glm::vec3(1.0f, 0.0f, 0.0f));
-        view = glm::translate(view, glm::vec3(-5.0f, -3.0f, -7.0f));
-		view = glm::rotate(view, -45.3f, glm::vec3(1.0f, 0.0f, 0.0f));
-        projection = glm::perspective(45.0f, 1200.0f / 1000.0f, 0.1f, 100.0f);
-        // Get their uniform location
-        GLint modelLoc = glGetUniformLocation(shaderProgram.Program, "model");
-        GLint viewLoc = glGetUniformLocation(shaderProgram.Program, "view");
-        GLint projLoc = glGetUniformLocation(shaderProgram.Program, "projection");
-	//		 glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-        // Pass the matrices to the shader
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-        // Note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
-        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
-
-	glBindVertexArray(VAO);
-	for( GLuint j=0; j<10; j++ ){
-	for (GLuint i = 0; i < 10; i++)
-        {
-            // Calculate the model matrix for each object and pass it to shader before drawing
-            glm::mat4 model;
-            model = glm::translate(model, glm::vec3((float)i, (float)j, 0.0f));
-			//model = glm::rotate(model, 30.0f, glm::vec3(1.0f, 0.0f, 0.0f));
-            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-
-            glDrawArrays(GL_TRIANGLES, 0, 6);
-        }
-	}
-	glDrawArrays(GL_TRIANGLES, 0, 6);
-	glBindVertexArray(0); 
-	glutSwapBuffers();
-}
 
 
 int main (int argc, char** argv) {
@@ -155,9 +61,7 @@ int main (int argc, char** argv) {
     glEnable(GL_DEPTH_TEST);
 
 	Shader shaderProgram("vshader.txt","fshader.txt");
-
-	//Kompilacja vertex shader
-	
+	Model ourModel("tank.obj");
 		// tutaj renderowanie
 
 	//koordynaty wierzcholkow
@@ -184,8 +88,8 @@ int main (int argc, char** argv) {
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
+   // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     // Position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
@@ -213,6 +117,7 @@ int main (int argc, char** argv) {
     glBindTexture(GL_TEXTURE_2D, 0); // Unbind texture when done, so we won't accidentily mess up our texture.
 	while (!glfwWindowShouldClose(window))
     {
+		
 			// Calculate deltatime of current frame
         GLfloat currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
